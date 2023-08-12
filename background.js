8c0417595a562c106fb877a51a02dacc667405cd
iregazzoli <ignacioregazzoli@gmail.com>
2023-08-12 17:56:45 -0300

    Manage to made background dragable

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
-const stageWidth = 800;
-const stageHeight = 600;
+const stageWidth = 900;
+const stageHeight = 800;
+const backgroundImageSrc = "assets/background-3.png";
+
+const log = console.log;
+
+const desiredBackgroundWidth = 4000;
+const desiredBackgroundHeight = 4000;
 
 const stage = new Konva.Stage({
-  container: "container",
+  container: "skillTreeMainCanvas",
   width: stageWidth,
   height: stageHeight,
 });
@@ -10,36 +16,42 @@ const stage = new Konva.Stage({
 const layer = new Konva.Layer();
 stage.add(layer);
 
-const backgroundImage = new Konva.Image({
-  image: new Image(), // Create a new Image object to load the image
-  width: 471, // Replace with the width of your background image
-  height: 472, // Replace with the height of your background image
-});
+const backgroundImage = new Image(); // Create a new Image object
+backgroundImage.onload = function () {
+  const canvas = document.createElement("canvas");
+  canvas.width = desiredBackgroundWidth;
+  canvas.height = desiredBackgroundHeight;
+
+  const context = canvas.getContext("2d");
+
+  const pattern = context.createPattern(backgroundImage, "repeat");
+  context.fillStyle = pattern;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  const konvaBackgroundImage = new Konva.Image({
+    image: canvas,
+    width: desiredBackgroundWidth,
+    height: desiredBackgroundHeight,
+  });
+
+  layer.add(konvaBackgroundImage);
+  layer.draw();
+
+  stage.on("dragmove", (e) => {
+    const minX = 0;
+    const minY = 0;
+    const maxX = -desiredBackgroundWidth + stageWidth;
+    const maxY = -desiredBackgroundHeight + stageHeight;
+
+    let boundedX = Math.min(stage.x(), minX);
+    boundedX = Math.max(boundedX, maxX);
+    let boundedY = Math.min(stage.y(), minY);
+    boundedY = Math.max(boundedY, maxY);
+
+    stage.position({ x: boundedX, y: boundedY });
+  });
+  stage.draggable(true);
+};
 
 // Load the background image
-backgroundImage.image.src = "assets/phos.png";
-
-const numRepetitionsX = Math.ceil(stageWidth / backgroundImage.width());
-const numRepetitionsY = Math.ceil(stageHeight / backgroundImage.height());
-
-for (let x = 0; x < numRepetitionsX; x++) {
-  for (let y = 0; y < numRepetitionsY; y++) {
-    const backgroundClone = backgroundImage.clone({
-      x: x * backgroundImage.width(),
-      y: y * backgroundImage.height(),
-    });
-    layer.add(backgroundClone);
-  }
-}
-
-layer.draw();
-
-stage.on("dragmove", (e) => {
-  const dx = e.evt.movementX;
-  const dy = e.evt.movementY;
-  layer.x(layer.x() + dx);
-  layer.y(layer.y() + dy);
-  layer.draw();
-});
-
-stage.draggable(true);
+backgroundImage.src = backgroundImageSrc;
